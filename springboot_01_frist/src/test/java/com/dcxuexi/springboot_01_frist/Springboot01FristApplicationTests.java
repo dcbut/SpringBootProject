230a641ec8128fcd123ca5187d2ae37f7ff0567b
Dchuang <dc.dongchuang@qq.com>
2022-11-17 22:48:47 +0800

    Initial commit

---
+++ springboot_01_frist/src/test/java/com/dcxuexi/springboot_01_frist/Springboot01FristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcxuexi.springboot_01_frist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01FristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ springboot_01_third/src/main/java/com/dcxuexi/Springboot01ThirdApplication.java
@@ -0,0 +1,13 @@
+package com.dcxuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01ThirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01ThirdApplication.class, args);
+	}
+
+}
